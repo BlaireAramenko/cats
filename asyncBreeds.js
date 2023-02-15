818742dcc2b74efae618417f92454925c78cddbb
BlaireAramenko <blaire.aramenko21@gmail.com>
2023-02-15 09:44:24 -0800

    added

---
+++ asyncBreeds.js
@@ -0,0 +1,18 @@
+const fs = require('fs');
+
+const breedDetailsFromFile = function(breed, functionToRunWhenThingsAreDone) {
+  console.log('breedDetailsFromFile: Calling readFile...');
+  fs.readFile(`./data/${breed}.txt`, 'utf8', (error, data) => {
+    // CHANGE: Pass data into callback instead of returning it directly
+    console.log("In readFile's Callback: it has the data.");
+    if (!error) functionToRunWhenThingsAreDone(data);
+  });
+};
+
+// CHANGE 1: Moved the console.log into a new function:
+const printOutCatBreed = breed => {
+  console.log('Return Value: ', breed) // => print out details correctly.
+};
+
+// CHANGE 2: we're now passing two arguments into breedDetailsFromFile: breed string and a callback function
+breedDetailsFromFile('Bombay', printOutCatBreed);
